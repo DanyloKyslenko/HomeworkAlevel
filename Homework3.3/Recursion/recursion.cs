3e3cd840ec9530ae56dc62586c17e51187be7d2f
DanyloKyslenko <142528207+DanyloKyslenko@users.noreply.github.com>
2023-10-28 00:59:59 +0300

    second commit

---
+++ Homework3.3/Recursion/recursion.cs
@@ -0,0 +1,18 @@
+﻿namespace Recursion
+{
+    public class FactorialAndFibonachi
+    {
+        public int Factorial(int n)
+        {
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+        public int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1) return n;
+
+            return Fibonachi(n - 1) + Fibonachi(n - 2);
+        }
+    }
+}
